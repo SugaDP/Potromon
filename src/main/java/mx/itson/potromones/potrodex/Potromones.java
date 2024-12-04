d7ed22e2b5e72118673cecf88ac5f694437aa88f
Jacarial <jan.rios253031@potros.itson.edu.mx>
2024-12-04 08:23:11 -0700

    ProyectoFinal ProIII

---
+++ src/main/java/mx/itson/potromones/potrodex/Potromones.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.potromones.potrodex;
+
+/**
+ *
+ * @author Genius Lab
+ */
+public class Potromones {
+
+    public static void main(String[] args) {
+    }
+}
